72983c2ee02d4beb1fecad733df0908f91c28fe9
Daniel Cuestas <102336853+dancuest@users.noreply.github.com>
2026-05-13 11:40:03 -0500

    Despliegue en render

--- app/src/main/java/com/example/animedev20/ui/theme/data/remote/ApiConfig.kt
+++ app/src/main/java/com/example/animedev20/ui/theme/data/remote/ApiConfig.kt
@@ -6,7 +6,7 @@ import java.util.Locale
 object ApiConfig {
     private const val ANDROID_EMULATOR_BASE_URL = "http://10.0.2.2:3000/"
     private const val GENYMOTION_BASE_URL = "http://10.0.3.2:3000/"
-    private const val DEVICE_BASE_URL = "https://aviator-dinner-smugly.ngrok-free.dev/"
+    private const val DEVICE_BASE_URL = "https://animedev-backend.onrender.com/"
 
     val baseUrl: String
         get() = when {
